04a56a7a9bc0504fcb05571519bc8fe0115e5268
Vlad Moldoveanu <vmoldoveanu@deloittece.com>
2019-04-08 18:26:31 +0300

    remove unused files

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -7,25 +7,26 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
 
 
-  view: any[] = [120, 120];
+    view: any[] = [120, 120];
 
-  single = [
-    {
-      "name": "Germany",
-      "value": 60
-    },
-    {
-      "name": "USA",
-      "value": 40
-    }
-  ];
-  colorScheme = {
-    domain: ['#d2d2d2', '#ffffff']
-  };
+    single = [
+        {
+        "name": "Germany",
+        "value": 60
+        },
+        {
+        "name": "USA",
+        "value": 40
+        }
+    ];
 
-  projects = [{ name: "HORNET", date: "11.05.2019", currentStaffing: "24", maxStaffing: "25"},
-              { name: "PRU", date: "11.05.2019", currentStaffing: "11", maxStaffing: "14"},
-              { name: "CYCLONES", date: "11.05.2019", currentStaffing: "18", maxStaffing: "25"}]
+    colorScheme = {
+        domain: ['#d2d2d2', '#ffffff']
+    };
+
+    projects = [{ name: "HORNET", date: "11.05.2019", currentStaffing: "24", maxStaffing: "25"},
+                { name: "PRU", date: "11.05.2019", currentStaffing: "11", maxStaffing: "14"},
+                { name: "CYCLONES", date: "11.05.2019", currentStaffing: "18", maxStaffing: "25"}]
 
 
 }
