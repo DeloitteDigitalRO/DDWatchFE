59e21015ef1285131030a4175bf62e4aa3565b39
Bors Nicolae <nbors@deloittece.com>
2019-06-26 16:33:49 +0300

    Added charts for project metrics

--- src/app/pages/project-delivery/project-delivery.component.ts
+++ src/app/pages/project-delivery/project-delivery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Project } from '../../@shared/models/project.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'project-delivery',
@@ -10,14 +11,58 @@ import { Project } from '../../@shared/models/project.model';
 export class ProjectDeliveryComponent implements OnInit {
   private project: Project;
   private latestMetricsReport;
+  metrics: Object;
+  objectKeys = Object.keys;
+
+  //chart  options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = false;
+  showXAxisLabel = false;
+  xAxisLabel = '';
+  showYAxisLabel = false;
+  yAxisLabel = '';
+  timeline = true;
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+  autoScale = true;
+  disabledMetrics = ['attritionStatus', 'branchCoverageStatus', 'changeOrderStatus', 'updateDate', 'changeOrderValue', 'createdOn', 'deliveryReportId', 'deliveryStatus', 'duplicationDensityStatus', 'empireTimeStatus', 'escalationRootCauseValue', 'escalationSeniorityValue', 'escalationStatus', 'id', 'invoicingStatus', 'invoicingValue', 'riskOverallStatus', 'riskOverallValue', 'testCoverageStatus']
+
 
   constructor(private router: Router) {
     this.project = this.router.getCurrentNavigation().extras as Project;
-    this.latestMetricsReport =  this.project.deliveryReports[0];
+    this.latestMetricsReport = this.project.deliveryReports[0];
+    this.initChartData();
     console.log('Project delivery for project', this.project);
   }
 
+
   ngOnInit() {
   }
+
+  initChartData() {
+    //loop through all the metrics and assign them as data arrays
+    //this.metrics = {Object.assign({}, activeProject.qualityReports[0].sonarQubeReport)};
+    this.metrics = {};
+    for (var key in this.project.deliveryReports[0].metricsReport) {
+      if (this.disabledMetrics.indexOf(key) === -1) {
+        this.metrics[key] = [{ name: key, series: [] }]
+      }
+    }
+
+    this.project.deliveryReports.reverse().forEach(deliveryReport => {
+      const date = moment(deliveryReport.updateDate).format('DD MM')
+      for (var _key in deliveryReport.metricsReport) {
+        if (this.disabledMetrics.indexOf(_key) === -1 && deliveryReport.metricsReport[_key] != null) {
+          this.metrics[_key][0].series.push({ name: date, value: deliveryReport.metricsReport[_key] });
+        }
+      }
+    });
+
+    console.log(this.metrics)
+  }
+
 
 }
