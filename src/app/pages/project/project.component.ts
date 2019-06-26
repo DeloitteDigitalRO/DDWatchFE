6d63fbf3c77f284ff1a61d2c7b64ed9d667c3ae9
Bors Nicolae <nbors@deloittece.com>
2019-06-26 14:04:31 +0300

    code reformat

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -4,7 +4,6 @@ import { ProjectService } from '../../@shared/services/project.service';
 import { Project } from '../../@shared/models/project.model';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
-import { RepositionScrollStrategy } from '@angular/cdk/overlay';
 @Component({
   selector: 'ngx-project',
   templateUrl: './project.component.html',
@@ -44,16 +43,16 @@ export class ProjectComponent {
             // get current report based on param ID
             if (params.has('id') && this.projectService.projects) {
                 this.projectList = this.projectService.projects;
-                this.activeProject = this.projectList.length > 0 && this.projectService.projects.find((el) => el.id === this.activeProjectId)
-                this.activeProjectRepo = this.activeProject.projectRepos.find(repo => repo.isDefault) || this.activeProject.projectRepos[0];
+                this.activeProject = this.getActiveProject();
+                this.activeProjectRepo = this.getActiveRepo();
                 this.initChartData();
             }
             // if page is accessed directly then fetch the data then filter the current report
             else {
                 this.projectService.getProjects().then((result) => {
                     this.projectList = result as Array<Project>;
-                    this.activeProject = this.projectList.length > 0 && this.projectService.projects.find((el) => el.id === this.activeProjectId)
-                    this.activeProjectRepo = this.activeProject.projectRepos.find(repo => repo.isDefault) || this.activeProject.projectRepos[0];
+                    this.activeProject = this.getActiveProject();
+                    this.activeProjectRepo = this.getActiveRepo();
                     this.initChartData();
                 })
             }
@@ -69,23 +68,20 @@ export class ProjectComponent {
                 this.metrics[key] = [{name: key, series:[]}]
             }
         }           
-        
-        for(var _i = this.activeProjectRepo.qualityReports.length - 1; _i >= 0; _i--){
-                var sonarReport = this.activeProjectRepo.qualityReports[_i];
-                const date = moment(sonarReport.updateDate).format('DD MM')
-                for (var _key in sonarReport.sonarQubeReport) {
-                    if(this.disabledMetrics.indexOf(_key) === -1){
-                        this.metrics[_key][0].series.push({name: date, value: sonarReport.sonarQubeReport[_key]});
-                    }
+
+        this.activeProjectRepo.qualityReports.slice().reverse().forEach(sonarReport => {
+            const date = moment(sonarReport.updateDate).format('DD MM')
+            for (var _key in sonarReport.sonarQubeReport) {
+                if(this.disabledMetrics.indexOf(_key) === -1){
+                    this.metrics[_key][0].series.push({name: date, value: sonarReport.sonarQubeReport[_key]});
                 }
-        }
+            }
+        });
     
         console.log(this.metrics)
     }
 
-    onChange(newValue) {
-        console.log(newValue);
-       
+    onActiveRepoChange(newValue) {
         this.activeProjectRepo =  newValue;
         this.initChartData();
     }
@@ -134,5 +130,17 @@ export class ProjectComponent {
         });
 
         return (total/counter).toFixed(1);
+    }
+
+    getActiveRepo(){
+        return this.activeProject.projectRepos.find(repo => repo.isDefault) || this.activeProject.projectRepos[0];
+    }
+
+    getActiveProject(){
+        return this.projectList.length > 0 && this.projectService.projects.find((el) => el.id === this.activeProjectId)
+    }
+
+    getActiveSonarQubeReport(){
+        return this.activeProjectRepo.qualityReports[0].sonarQubeReport;
     }
 }
